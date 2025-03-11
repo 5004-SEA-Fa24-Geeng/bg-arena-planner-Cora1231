b51f8f7ede9e98b3e84c1a35e63de5af84978f69
qianfupeng <peng.qianf@northeastern.edu>
2025-03-10 19:43:55 -0700

    first patch

--- src/main/java/student/Filters.java
+++ src/main/java/student/Filters.java
@@ -108,6 +108,7 @@ public class Filters {
                 }
             }
             else{
+                System.out.print("this is value "+value);
                 for (BoardGame s : list) {
                     if (s.getName().trim().toLowerCase().replaceAll("\\s+", "").contains(value.toLowerCase().replaceAll("\\s+", ""))) {
                         res.add(s);
